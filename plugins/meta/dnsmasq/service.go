689ec23e56a2e1c9a14b9901273d9ba7ba17b9cf
baude <bbaude@redhat.com>
2019-08-25 09:57:01 -0500

    add dnsmasq plugin
    the dnsmasq plugin is designed to help with resolving names between pods
    or containers. it uses a dnsmasq instance that only listens on the
    network interface in question.  otherwise, the dnsmasq instance will act
    as a caching server to resolve "outside" requests.
    
    Signed-off-by: baude <bbaude@redhat.com>

---
+++ plugins/meta/dnsmasq/service.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"golang.org/x/sys/unix"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+// newDNSMasqFile creates a new instance of a dnsMasqFile
+func newDNSMasqFile(domainName, networkInterface, networkName string) (dnsMasqFile, error){
+	dnsMasqBinary, err := exec.LookPath("dnsmasq")
+	if err != nil {
+		return dnsMasqFile{}, errors.New("the dnsmasq cni plugin requires the dnsmasq binary be in PATH")
+	}
+	masqConf := dnsMasqFile{
+		ConfigFile:       makePath(networkName, confFileName),
+		Domain:           domainName,
+		PidFile:          makePath(networkName, pidFileName),
+		NetworkInterface: networkInterface,
+		AddOnHostsFile:   makePath(networkName, hostsFileName),
+		Binary:           dnsMasqBinary,
+	}
+	return masqConf, nil
+}
+
+// hup sends a sighup to a running dnsmasq to reload its hosts file. if
+// there is no instance of the dnsmasq, then it simply starts it.
+func (d dnsMasqFile) hup() error {
+	// First check for pidfile; if it does not exist, we just
+	// start the service
+	if _, err := os.Stat(d.PidFile); os.IsNotExist(err) {
+		return d.start()
+	}
+	pid, err := d.getPidProcess()
+	if err != nil {
+		return err
+	}
+	if !isRunning(pid) {
+		return d.start()
+	}
+	return pid.Signal(unix.SIGHUP)
+}
+
+// isRunning sends a signal 0 to the pid to determine if it
+// responds or not
+func isRunning(pid *os.Process) (bool) {
+	if err := pid.Signal(syscall.Signal(0)); err != nil {
+		return false
+	}
+	return true
+}
+
+func (d dnsMasqFile) start() error {
+	args := []string{
+		"-u",
+		"root",
+		fmt.Sprintf("--conf-file=%s", d.ConfigFile),
+	}
+	cmd := exec.Command(d.Binary, args...)
+	return cmd.Run()
+}
+
+func (d dnsMasqFile) stop() error {
+	pid, err := d.getPidProcess()
+	if err != nil {
+		return err
+	}
+	return pid.Kill()
+}
+
+// getPidProcess reads the PID for the dnsmasq instance and returns it in the
+// form of an int
+func (d dnsMasqFile) getPidProcess() (*os.Process, error) {
+	pidFileContents, err := ioutil.ReadFile(d.PidFile)
+	if err != nil {
+		return nil, err
+	}
+	pid, err := strconv.Atoi(strings.TrimSpace(string(pidFileContents)))
+	if err != nil {
+		return nil, err
+	}
+	return os.FindProcess(pid)
+}
+
+// makePath formats a path name given a domain and suffix
+func makePath(networkName, fileName string) string {
+	// the generic path for where conf, host, pid files are kept is:
+	// /run/containers/cni/dnsmasq/<network-name>/
+	return filepath.Join(dnsMasqConfPath, networkName, fileName)
+}
